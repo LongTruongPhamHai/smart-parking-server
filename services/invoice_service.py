c1ca47298c6d190dedab415fa4d075c6d8f99690
LongTruongPhamHai <longtruongphamhai@gmail.com>
2026-01-07 18:09:44 +0700

    UPDATE

--- services/invoice_service.py
+++ services/invoice_service.py
@@ -16,55 +16,55 @@ class InvoiceService:
         self.user_repo = user_repo
         self.plot_repo = plot_repo
 
-    async def create_invoice(self, data):
-        """Quy trình Xe vào (Check-in)"""
-        plot = self.plot_repo.getPLotById(data.parking_lot_id)
-        if not plot or plot.status == "occupied":
-            raise ValueError("Vị trí đỗ không khả dụng")
+    # async def create_invoice(self, data):
+    #     """Quy trình Xe vào (Check-in)"""
+    #     plot = self.plot_repo.getPLotById(data.parking_lot_id)
+    #     if not plot or plot.status == "occupied":
+    #         raise ValueError("Vị trí đỗ không khả dụng")
 
-        invoice_dict = {
-            "user_id": data.user_id,
-            "parking_lot_id": data.parking_lot_id,
-            "start_time": datetime.utcnow(),
-            "end_time": None,
-            "duration": 0.0,
-            "total_price": 0.0,
-        }
+    #     invoice_dict = {
+    #         "user_id": data.user_id,
+    #         "parking_lot_id": data.parking_lot_id,
+    #         "start_time": datetime.utcnow(),
+    #         "end_time": None,
+    #         "duration": 0.0,
+    #         "total_price": 0.0,
+    #     }
 
-        self.plot_repo.updatePlot(data.parking_lot_id, {"status": "occupied"})
-        return self.invoice_repo.createInvoice(invoice_dict)
+    #     self.plot_repo.updatePlot(data.parking_lot_id, {"status": "occupied"})
+    #     return self.invoice_repo.createInvoice(invoice_dict)
 
-    async def complete_invoice(self, invoice_id: str):
-        """Quy trình Xe ra & Thanh toán (Check-out)"""
+    # async def complete_invoice(self, invoice_id: str):
+    #     """Quy trình Xe ra & Thanh toán (Check-out)"""
 
-        invoice = self.invoice_repo.getInvoiceById(invoice_id)
-        plot = self.plot_repo.getPLotById(invoice.parking_lot_id)
-        user = self.user_repo.getUserById(invoice.user_id)
+    #     invoice = self.invoice_repo.getInvoiceById(invoice_id)
+    #     plot = self.plot_repo.getPLotById(invoice.parking_lot_id)
+    #     user = self.user_repo.getUserById(invoice.user_id)
 
-        end_time = datetime.utcnow()
-        duration_delta = end_time - invoice.start_time
+    #     end_time = datetime.utcnow()
+    #     duration_delta = end_time - invoice.start_time
 
-        duration_hms = timedelta_to_hms(duration_delta)
+    #     duration_hms = timedelta_to_hms(duration_delta)
 
-        duration_hours = max(duration_delta.total_seconds() / 3600, 0.1)
-        total_price = round(duration_hours * plot.unit_price, 2)
+    #     duration_hours = max(duration_delta.total_seconds() / 3600, 0.1)
+    #     total_price = round(duration_hours * plot.unit_price, 2)
 
-        if user.balance < total_price:
-            raise ValueError(
-                f"Số dư không đủ. Cần {total_price}, hiện có {user.balance}"
-            )
+    #     if user.balance < total_price:
+    #         raise ValueError(
+    #             f"Số dư không đủ. Cần {total_price}, hiện có {user.balance}"
+    #         )
 
-        self.user_repo.updateBalance(user.id, round(user.balance - total_price, 2))
-        self.plot_repo.updatePlot(invoice.parking_lot_id, {"status": "available"})
+    #     self.user_repo.updateBalance(user.id, round(user.balance - total_price, 2))
+    #     self.plot_repo.updatePlot(invoice.parking_lot_id, {"status": "available"})
 
-        update_data = {
-            "end_time": end_time,
-            "duration": duration_hours,
-            "total_price": total_price,
-            "status": "Deactive",
-        }
+    #     update_data = {
+    #         "end_time": end_time,
+    #         "duration": duration_hours,
+    #         "total_price": total_price,
+    #         "status": "Deactive",
+    #     }
 
-        return self.invoice_repo.updateInvoice(invoice_id, update_data)
+    #     return self.invoice_repo.updateInvoice(invoice_id, update_data)
 
     async def get_all_invoices(self):
         """
